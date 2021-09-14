e9f2816ff3cd58fbcb93db0250f4b162a7ca85ec
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-14 14:07:56 +0900

    :sparkles: [master] add Spring Projects

---
+++ backend/BackSpring/src/test/java/com/pjt2/lb/BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt2.lb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
