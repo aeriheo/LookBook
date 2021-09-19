8c188727a8de64bb9daf1590120b47c4729fcfae
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-19 14:56:14 +0900

    :sparkles: [BE_YS] add django secret

---
+++ backend/BackDjango/BackDjango/secret.py
@@ -0,0 +1,14 @@
+DJANGO_SECRET = {
+    "SECRET_KEY": "django-insecure-=!eqy0-cs9k+6q(4%yxutz7ot7sr(8*hd15=!x&-5fxi$-(_h7"
+}
+
+LB_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_testDB',
+        'USER': 'lookbook',
+        'PASSWORD': '1234',
+        'HOST': '13.124.75.162',
+        'PORT': '3306'
+    }
+}
