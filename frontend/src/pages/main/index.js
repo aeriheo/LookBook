b756f0b95b9cf8a11a998970878df90366bc0345
허애리 <hureari@naver.com>
2021-09-24 15:24:44 +0900

    :sparkles: [FE] Header 개발

---
+++ frontend/src/pages/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/headers';
+
+const Main = props => {
+    return(
+        <div style={{height: '100%', display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
+            <Header  />
+        </div>
+    );
+};
+
+
+export default Main;
