8a8bcf34e625d6048933791348754ed0c8e5c07d
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-24 22:11:11 +0900

    :sparkles: [BE_YS] User JpaRepository 구현

---
+++ backend/BackSpring/src/main/java/com/pjt2/lb/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pjt2.lb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pjt2.lb.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findUserByUserEmail(String userEmail);
+	User findUserByUserNickname(String userNickname);
+}
