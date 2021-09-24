6fe4e5b141dea9c30bdaeaed0205e8ff22e61d6f
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-24 22:12:48 +0900

    :sparkles: [BE_YS] 로그인 구현

---
+++ backend/BackSpring/src/main/java/com/pjt2/lb/controller/AuthController.java
@@ -0,0 +1,92 @@
+package com.pjt2.lb.controller;
+
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pjt2.lb.common.util.JwtTokenUtil;
+import com.pjt2.lb.entity.User;
+import com.pjt2.lb.request.UserLoginPostReq;
+import com.pjt2.lb.response.UserLoginPostRes;
+import com.pjt2.lb.service.UserService;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		String message = "Hello My name is AuthController";
+		return message;
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<UserLoginPostRes> login(@RequestBody UserLoginPostReq loginInfo) {
+		String userEmail = loginInfo.getUserEmail();
+		String userPassword = loginInfo.getUserPassword();
+		
+		try {
+			
+			User user = userService.getUserByUserEmail(userEmail);
+			String accessToken = JwtTokenUtil.getToken(userEmail);
+			String refreshToken = JwtTokenUtil.getRefreshToken(userEmail);
+			System.out.println("accessToken : " + JwtTokenUtil.getToken(userEmail));
+			System.out.println("refreshToken : " + JwtTokenUtil.getRefreshToken(userEmail));
+			
+			JwtTokenUtil.handleError(accessToken);
+			
+			if (userPassword.equals(user.getUserPassword())) {
+				return ResponseEntity.status(200).
+						body(new UserLoginPostRes(200, "로그인에 성공하였습니다.", JwtTokenUtil.getToken(userEmail), JwtTokenUtil.getRefreshToken(userEmail)));
+			}
+			else {
+				return ResponseEntity.status(401).body(new UserLoginPostRes(401,"잘못된 비밀번호 입니다.", null, null));
+			}
+		} catch(NullPointerException e) {
+			return ResponseEntity.status(404).body(new UserLoginPostRes(404, "존재하지 않는 계정입니다.", null, null));
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
