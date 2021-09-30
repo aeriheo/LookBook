23c15826cf379d9c79c7a12dc5bd166d89ad8678
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-30 10:17:10 +0900

    :recycle: [Dev] kakao redirect_url 변경 취소

--- frontend/src/oauth/index.js
+++ frontend/src/oauth/index.js
@@ -1,5 +1,4 @@
 const CLIENT_ID = 'a765ac439be73b3505f709a713a0dcd0';
-const REDIRECT_URI = 'https://j5a502.p.ssafy.io/joinsocial';
-// const REDIRECT_URI = 'http://localhost:3000/joinsocial';
+const REDIRECT_URI = 'http://localhost:3000/joinsocial';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
