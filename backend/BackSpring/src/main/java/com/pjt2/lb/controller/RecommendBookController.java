810099c86b834a4760e54ff87b506a53b819ecac
smy999 <210113yynim@naver.com>
2021-10-02 16:42:44 +0900

    :sparkles: [Dev] Add RecommendBookController

---
+++ backend/BackSpring/src/main/java/com/pjt2/lb/controller/RecommendBookController.java
@@ -0,0 +1,20 @@
+package com.pjt2.lb.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(
+        origins = {"http://localhost:3000", "https://j5a502.p.ssafy.io/"},
+        allowCredentials = "true", 
+        allowedHeaders = "*", 
+        methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT,RequestMethod.OPTIONS}
+)
+@RequestMapping("/recommends")
+@RestController
+public class RecommendBookController {
+
+	
+	
+}
