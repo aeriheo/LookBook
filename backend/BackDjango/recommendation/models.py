79c82e67ef86d2b5ed01f5f9ae11207446ac101b
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-02 18:54:31 +0900

    :bug: [BE_YS] modify Model Error

--- backend/BackDjango/recommendation/models.py
+++ backend/BackDjango/recommendation/models.py
@@ -1,23 +1,35 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-class UserBasedCFModel(models.Model):
-    user_email = models.CharField(max_length=100)
-    book_isbn = models.CharField(max_length=13)
+from book.models import Book
+from user.models import User
 
-    class Meta:
-        managed = False
-        db_table = 'UserBasedCFModel'
 
 class ItemBasedCFModel(models.Model):
-    book_isbn = models.CharField(max_length=13)
+    book_isbn = models.ForeignKey(Book, related_name="ItemBasedCFModel", on_delete=models.CASCADE, db_column='book_isbn')
 
     class Meta:
         managed = False
         db_table = 'ItemBasedCFModel'
 
+class UserBasedCFModel(models.Model):
+    user_email = models.ForeignKey(User, related_name="UserBasedCFModel", on_delete=models.CASCADE, db_column='user_email')
+    book_isbn = models.ForeignKey(Book, related_name="UserBasedCFModel", on_delete=models.CASCADE, db_column='book_isbn')
+
+    class Meta:
+        managed = False
+        db_table = 'UserBasedCFModel'
+
+#
 class UserPredictedGradeModel(models.Model):
-    user_email = models.CharField(max_length=100)
-    book_isbn = models.CharField(max_length=13)
+    user_email = models.ForeignKey(User, related_name="UserPredictedGradeModel", on_delete=models.CASCADE, db_column='user_email')
+    book_isbn = models.ForeignKey(Book, related_name="UserPredictedGradeModel", on_delete=models.CASCADE, db_column='book_isbn')
 
     class Meta:
         managed = False
--- backend/BackDjango/recommendation/serializers.py
+++ backend/BackDjango/recommendation/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from recommendation.models import UserBasedCFModel
+from recommendation.models import UserBasedCFModel, ItemBasedCFModel, UserPredictedGradeModel
 
 class UserBasedCFSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserBasedCFModel
+        fields = "__all__"
+
+class ItemBasedCFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemBasedCFModel
+        fields = "__all__"
+
+class UserPredictedGradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPredictedGradeModel
         fields = "__all__"
