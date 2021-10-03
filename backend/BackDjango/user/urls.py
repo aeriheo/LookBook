0c81193bb63e7d1d0cc33c145bc8c33ce3fd068c
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-03 16:38:15 +0900

    :recycle: [BE_YS] modify User MVC

--- backend/BackDjango/user/urls.py
+++ backend/BackDjango/user/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserListViewSet.as_view(), name="UserListViewSet")
+    path('', views.UserListViewSet.as_view(), name="UserListViewSet"),
+    path('user/<user_email>', views.UserDetailListViewSet.as_view(), name="UserDetailListViewSet")
 ]
