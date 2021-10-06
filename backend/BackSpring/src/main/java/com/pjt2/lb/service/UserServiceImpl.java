675fe3bf85f1f0482c4cb01f1475437e8b64f561
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-06 23:15:56 +0900

    :recycle: [Dev] 회원가입시 userjoinType 추가

--- backend/BackSpring/src/main/java/com/pjt2/lb/service/UserServiceImpl.java
+++ backend/BackSpring/src/main/java/com/pjt2/lb/service/UserServiceImpl.java
@@ -36,7 +36,7 @@ public class UserServiceImpl implements UserService {
 		user.setUserPassword(passwordEncoder.encode(userRegisterInfo.getUserPassword()));
 		user.setUserName(userRegisterInfo.getUserName());
 		user.setUserNickname(userRegisterInfo.getUserNickname());
-		user.setUserJoinType(1);
+		user.setUserJoinType(userRegisterInfo.getUserJoinType());
 		
 		return userRepository.save(user);
 	}
